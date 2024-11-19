a76cef5c64e7f151b35bcffc26efccb396d63d50
hw <hw710204@gmail.com>
2024-11-19 17:58:20 +0900

    frontend - 토큰 활용 로그인 구현

--- frontend/src/ApiService.js
+++ frontend/src/ApiService.js
@@ -1,15 +1,26 @@
 import { API_BASE_URL } from "./api-config";
 
 export function call(api, method, request) {
+  //헤더 정의
+  let headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  //로컬 스토리지에서 ACCESS TOKEN 가져와서 헤더에 세팅
+  const accessToken = localStorage.getItem("ACCESS_TOKEN");
+  if (accessToken && accessToken != null) {
+    headers.append("Authorization", "Bearer " + accessToken);
+  }
+
+  //헤더 추가
   let options = {
-    headers: new Headers({
-      "Content-Type": "application/json",
-    }),
+    headers: headers,
     url: API_BASE_URL + api,
     method: method,
   };
 
   if (request) {
+    //GET method
     options.body = JSON.stringify(request);
   }
 
@@ -21,11 +32,27 @@ export function call(api, method, request) {
         //403 에러가 뜰 때
         window.location.href = "/login"; // '/login' 으로 리다이렉트할 것
       } else {
-        Promise.reject(response);
-        throw Error(response);
+        new Error(response);
       }
     })
     .catch((error) => {
       console.error("http error: ", error);
     });
+}
+
+export function signin(userDTO) {
+  return call("/auth/signin", "POST", userDTO).then((response) => {
+    //alert("로그인 토큰: " + response.token);  -> 디버깅용, 로그인 토큰 잘 넘어오는지 확인 위함
+    if (response.token) {
+      //로컬 스토리지에 토큰 저장
+      localStorage.setItem("ACCESS_TOKEN", response.token);
+      // token 존재 시 Todo 화면으로 리다이렉트
+      window.location.href = "/";
+    }
+  });
+}
+
+export function signout() {
+  localStorage.setItem("ACCESS_TOKEN", null);
+  window.location.href = "/login";
 }
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,7 +1,64 @@
 import React from "react";
+import { Container, Grid2, Typography, TextField, Button } from "@mui/material";
+import { signin } from "./ApiService";
 
-const Login = () => {
-  return <p> 로그인 페이지</p>;
-};
+function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const username = data.get("username");
+    const password = data.get("password");
+
+    signin({ username: username, password: password });
+  };
+
+  return (
+    // 로그인 컴포넌트
+    <Container comporent="main" maxWidth="xs" style={{ marginTop: "8%" }}>
+      <Grid2 container spacing={2}>
+        <Grid2 xs={12}>
+          <Typography comporent="h1" variant="h5">
+            로그인
+          </Typography>
+        </Grid2>
+      </Grid2>
+
+      <form noValidate onSubmit={handleSubmit}>
+        {" "}
+        {/* submit 버튼을 누르면 handleSubmit 실행*/}
+        <Grid2 container spacing={2}>
+          <Grid2 xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              id="username"
+              label="아이디"
+              name="username"
+              autoComplete="username"
+            />
+          </Grid2>
+          <Grid2 xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              id="password"
+              label="비밀번호"
+              type="password"
+              name="password"
+              autoComplete="current-password"
+            />
+          </Grid2>
+          <Grid2>
+            <Button type="submit" fullWidth variant="contained" color="primary">
+              로그인
+            </Button>
+          </Grid2>
+        </Grid2>
+      </form>
+    </Container>
+  );
+}
 
 export default Login;
